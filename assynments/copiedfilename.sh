cd37523a763c3fd7ae41a0799dc9bc48048119e2
Ifeoma Anyanwu <ifymmadu2002@yahoo.com>
2022-05-23 21:24:09 +0000

    adding git bash team assynments

---
+++ assynments/copiedfilename.sh
@@ -0,0 +1,10 @@
+
+#!/bin/bash
+# This script will accept user input and display them
+echo -n "Enter your  name: "
+read name
+echo -n "Enter your age: "
+read age
+echo Hello $name. Your are $age yeras old.
+echo Thank you
+
